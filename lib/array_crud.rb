91ce55d133778fcefa395073a47a549674fe253d
Bailey Wood <woodbmae@gmail.com>
2019-09-17 19:30:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,23 +23,14 @@ def remove_element_from_end_of_array(array)
   array.pop
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 def remove_element_from_start_of_array(array)
-  foods = ["bacon", "eggs", "cheese"]
-  foods.shift
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.shift
 end
+
+
+
+
 
 def retrieve_element_from_index(array, index_number)
   foods = ["bacon", "eggs", "cheese"]
