a662db35ba910b80b294a8588e6678361e80dd47
Bailey Wood <woodbmae@gmail.com>
2019-09-17 19:18:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  foods = ["bacon", "eggs", "cheese"]
+  foods = ["bacon", "eggs", "cheese", "bread"]
 end
 
 def add_element_to_end_of_array(array, element)
