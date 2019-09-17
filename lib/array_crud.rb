739402208adab20206db64a858525eb22fdaf18d
Bailey Wood <woodbmae@gmail.com>
2019-09-17 19:14:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   foods = ["bacon", "eggs", "cheese"]
-  foods << 
+  foods << "bread"
 end
 
 def add_element_to_start_of_array(array, element)
